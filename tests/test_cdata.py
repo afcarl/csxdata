fbc46903bc87a4a3e5021b0788db97cc9162b170
Csxeba <csxeba@gmail.com>
2016-08-31 18:23:19 +0200

    Renamed tests for more clarity

--- tests/test_cdata.py
+++ tests/test_cdata.py
@@ -37,7 +37,7 @@ class TestCData(unittest.TestCase):
 
         self.data = CData((self.X_, self.y_), cross_val=0)
 
-    def test_initialization(self):
+    def test_initialization_on_etalon_with_given_parameters(self):
         new_data = CData((self.X_, self.y_), cross_val=0.5)
         self.assertEqual(new_data.embedding, "onehot",
                          "<embedding> property is faulty after initialization!")
@@ -55,23 +55,23 @@ class TestCData(unittest.TestCase):
     def test_reset(self):
         data2 = CData((self.X_, self.y_), cross_val=0)
         er = "Difference detected in data shapes"
+        # given
         self.data.crossval = 3
         self.data.embedding = 10
         self.data.transformation = ("pca", 1)
+        # when
         self.data.reset_data(shuff=False)
-        self.data.crossval = 0
-        self.assertEqual(self.data.learning.shape, (10, 3), msg=er)
-        self.assertEqual(self.data.learning.shape, data2.learning.shape, msg=er)
         sm1, sm2 = np.sum(self.data.data), np.sum(data2.data)
-        self.assertEqual(sm1, sm2,
-                         msg="The sums of learning data differ by {}!\n{}\n{}"
+        # then
+        self.assertEqual(self.data.learning.shape, (7, 3), msg=er)
+        self.assertEqual(sm1, sm2, msg="The sums of learning data differ by {}!\n{}\n{}"
                          .format(abs(sm1 - sm2), sm1, sm2))
 
-    def test_writability(self):
+    def test_core_data_is_readonly(self):
         with self.assertRaises(ValueError):
             self.data.data[0][0] = 2.0
 
-    def test_splitting(self):
+    def test_setter_sets_crossval_getter_right(self):
         self.data.crossval = 5
         self.assertEqual(self.data.crossval, 0.5,
                          "Wrong <crossval> value in data!")
@@ -82,17 +82,17 @@ class TestCData(unittest.TestCase):
         self.assertEqual(self.data.n_testing, self.data.testing.shape[0],
                          "Validation data splitting went wrong @ testing!")
 
-    def test_weighing(self):
+    def test_weights_sum_to_N(self):
         w = self.data.sample_weights
         self.assertEqual(round(w.sum()), self.data.N)
 
-    def test_concatenate(self):
+    def test_concatenate_produces_right_shape(self):
         newdata = CData((self.X_, self.y_), cross_val=0)
         newdata.concatenate(self.data)
         self.assertEqual(newdata.N, 20, "Split after concatenation went wrong!")
         self.assertEqual(newdata.data.shape, (20, 3), "Shapes went haywire after concatenation!")
 
-    def test_batch_generator(self):
+    def test_batches_from_generator_are_shaped_and_distributed_right(self):
         i = 0
         self.data.crossval = 2
         self.data.transformation = ("ae", 15)
@@ -104,13 +104,13 @@ class TestCData(unittest.TestCase):
             i += 1
         self.assertEqual(i, 4, msg="Number of batches differ. Got {} expected {}".format(i, 4))
 
-    def test_inputs_outputs_vanilla(self):
+    def test_neurons_required_property_on_untransformed_data(self):
         inshape, outputs = self.data.neurons_required
         self.assertIsInstance(inshape, int, "<Fanin> input shape is in a tuple!")
         self.assertEqual(inshape, 3)
         self.assertEqual(outputs, 3)
 
-    def test_inputs_outputs_ultimate(self):
+    def test_neurons_required_proprety_after_heavy_transformation_then_resetting(self):
         self.data.embedding = 10
         self.data.transformation = ("pca", 2)
         inshape, outputs = self.data.neurons_required
@@ -136,7 +136,7 @@ class TestTransformations(unittest.TestCase):
 
         self.data = CData((self.X_, self.y_), cross_val=0)
 
-    def test_standardization(self):
+    def test_standardization_on_etalon(self):
         self.data.reset_data(shuff=False)
 
         calcme = parse_csv(etalonroot + "std.csv", dtype="float64")[0]
@@ -150,7 +150,7 @@ class TestTransformations(unittest.TestCase):
                          "The transformation property is faulty!")
         self.assertTrue(np.all(np.equal(X, calcme)), "Standardization is faulty!")
 
-    def test_pca(self):
+    def test_pca_on_etalon(self):
         self.data.reset_data(shuff=False)
 
         calcme = parse_csv(etalonroot + "pca.csv", dtype="float64")[0]
@@ -165,7 +165,7 @@ class TestTransformations(unittest.TestCase):
                          "The transformation property is faulty!")
         self.assertTrue(np.all(eq), "PCA is faulty!")
 
-    def test_autoencoding(self):
+    def test_autoencoding_on_etalon(self):
         self.data.reset_data(shuff=False)
         self.data.transformation = ("ae", 10)
         self.assertEqual(self.data.transformation, "autoencoding",
@@ -191,7 +191,7 @@ class TestEmbedding(unittest.TestCase):
 
         self.data = CData((self.X_, self.y_), cross_val=0)
 
-    def test_embedding(self):
+    def test_embedding_then_reverting_to_onehot_doesnt_break_shapes(self):
         self.data.reset_data(shuff=False)
         self.data.crossval = 0
         self.data.embedding = 10
