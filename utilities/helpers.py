38b182f5b00a6c9475e68bc67b8e88ff09b9da8b
Csxeba <csxeba@gmail.com>
2016-11-19 22:29:20 +0100

    RNN bias grads are wrong

--- utilities/helpers.py
+++ utilities/helpers.py
@@ -8,8 +8,8 @@ def speak_to_me(net, dat: Sequence, stochastic=False, ngrams=50):
     human = dat.translate(pred)
     chain = "[{}]".format("".join(human))
     for _ in range(ngrams):
-        ingoes = pred[:, -(dat.timestep - 1):, :]
-        nextpred = net.predict(ingoes)
+        inputs = pred[:, -(dat.timestep - 1):, :]
+        nextpred = net.prediction(inputs)
         pred = np.column_stack((pred, nextpred.reshape(1, *nextpred.shape)))
         human = dat.translate(nextpred, use_proba=stochastic)
         chain += "".join(human)
